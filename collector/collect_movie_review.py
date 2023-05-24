136f44794fbbeb289d06c3c1ccba612beb5b67b2
JEONG SUNWOO <sunwoojeong0430@gmail.com>
2023-05-24 12:30:31 +0900

    Add webcrawl(daum_news_page.py)

--- collector/collect_movie_review.py
+++ collector/collect_movie_review.py
@@ -1,4 +1,3 @@
-import requests
 import time
 import math
 import re
@@ -9,48 +8,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-
 # anaconda prompt
 # > conda activate cnu_python
 # > pip install beautifulsoup
 # > pip install selenium
 # > pip install webdriver_manager
 
-# Selenium: 동적 페이지에서 웹 크롤링 가능!
-#                -> 원래 용도: 웹 브라우저 테스트 용
 
-# http: 웹
-# ftp: 파일 전송
-# ssh: 터미널 접속
-# smtp: 메일 전송
+#######################################
+# 1.Install ChromeDriver for selenium #
+#######################################
+# selenium -> webdriver(chrome)
+#  1.최신버전 사용해서 code로 다운로드(최신버전)
+#  2.chrome driver 다운로드 후 주입(구버전)
+# 주소: https://sites.google.com/chromium.org/driver/
+
+driver = Service(executable_path="../driver/chromedriver.exe")
+options = Options()
+options.add_experimental_option("detach", True)  # ChromeDriver 자동 종료 X
+driver = webdriver.Chrome(service=driver, options=options)
+
+
 ##############################
 # 2.Open URL in ChromeDriver #
 ##############################
 movie_id = 160244
-url = f"https://movie.daum.net/moviedb/main?movieId={movie_id}"
+url = f"https://movie.daum.net/moviedb/grade?movieId={movie_id}"
 
 # Selenium의 ChromeDriver를 통해서 해당 URL 접속
 driver.get(url)
-time.sleep(1) # 1초 딜레이(웹 페이지 랜더링 완료까지 기다리기)
-doc_html = driver.page_source # 해당 URL 소스코드
+time.sleep(1)  # 1초 딜레이(웹 페이지 랜더링 완료까지 기다리기)
+doc_html = driver.page_source  # 해당 URL 소스 코드
 
 doc = BeautifulSoup(doc_html, "html.parser")
-title = doc.select("span.txt_tit")[1].text.strip() # 영화 제목 수집
-
-# 전체리뷰: 91개
-# 기존출력 : 19개
-# 클릭추가리뷰 : 30개
-# (전체리뷰-기존리뷰) / 30 - 3 (평점 더보기 클릭 횟수)
+title = doc.select("span.txt_tit")[1].text.strip()  # 영화 제목 수집
 
 total_review_txt = doc.select("span.txt_netizen")[0].text
 # 정규화 → 숫자만 추출
 total_review = int(re.sub(r"[^~0-9]", "", total_review_txt))
-click_cnt = math.ceil((total_review - 10) / 30) # "평점 더보기" 버튼 클린 횟수(모든 리뷰 출력을 위한)
+
+click_cnt = math.ceil((total_review - 10) / 30)  # "평점 더보기" 버튼 클릭 횟수(모든리뷰 출력을위한)
 
 for i in range(click_cnt):
     # "평점 더보기" 클릭(리뷰 30개씩 증가)
@@ -58,25 +55,15 @@ for i in range(click_cnt):
     time.sleep(1)
 time.sleep(5)
 
-# 해당 페이지에 모든 리뷰 출력 완료
+# >> 해당 페이지에 모든 리뷰 출력 완료
 review_html = driver.page_source
-print(review_html)
-doc = BeautifulSoup(review_html, "html_parser")
+doc = BeautifulSoup(review_html, "html.parser")
 review_list = doc.select("ul.list_comment div.cmt_info")
 
 for review_box in review_list:
     score = review_box.select("div.ratings")[0].text
-    writer = review_box.select("")
-    review_date = review_box.select("")
-    review = review_box.select("")
-    print(f" - 평점: {score}")
-
-#######################################
-# 1.install ChromeDriver for selenium #
-#######################################
-# Selenium -> webdriver(chrome)
-# 1.최신버전 사용해서 code로 다운로드(최신버전)
-# 2.chrome driver 다운로드 후 주입(구버전)
-# 주소: https://sites.google.com/chromium.org/driver/
-
-# 숙제: 리뷰, 작성자, 작성일자 수집!
+ #   writer = review_box.select("")
+ #   review_date = review_box.select("")
+ #   review = review_box.select("")
+ #   print(f" - 평점: {score}")
+    # 숙제: 리뷰, 작성자, 작성일자 수집!
